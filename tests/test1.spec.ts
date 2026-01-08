7a494651e98902b0254ec1f03495de6a9206ae95
januarcexp <mccoystar1818@gmail.com>
2026-01-09 01:09:12 +0700

    update file ke 3

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('update file dengan akun januar', async ({ page }) => {
+test('update file dengan akun januar 3', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
